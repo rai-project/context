1ce2ffadff7cb6a9781f922f420cacfcac15d944
Abdul Dakkak <abduld@wolfram.com>
2017-03-11 10:28:41 +0000

    add helper

--- context.go
+++ context.go
@@ -6,6 +6,18 @@ type Context struct {
 	context.Context
 }
 
+func New() Context {
+	return Background()
+}
+
+func Background() Context {
+	return Context{context.Background()}
+}
+
+func TODO() Context {
+	return Context{context.TODO()}
+}
+
 func (c *Context) WithValue(key, val interface{}) Context {
 	return Context{context.WithValue(c, key, val)}
 }
