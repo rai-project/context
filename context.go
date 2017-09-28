56d653980072603931bbcf2060af52239613ccc4
Abdul Dakkak <abduld@wolfram.com>
2017-09-28 08:50:26 -0500

    run gocmt https://github.com/Gnouc/gocmt

--- context.go
+++ context.go
@@ -2,18 +2,22 @@ package context
 
 import "context"
 
+// Context ...
 type Context struct {
 	context.Context
 }
 
+// Background ...
 func Background() Context {
 	return Context{context.Background()}
 }
 
+// TODO ...
 func TODO() Context {
 	return Context{context.TODO()}
 }
 
+// WithValue ...
 func (c Context) WithValue(key, val interface{}) Context {
 	return Context{context.WithValue(c, key, val)}
 }
